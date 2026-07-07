988d0913659e025a1160313c7cccae334ad66fca
samsu007 <itdev.nuansa@gmail.com>
2026-07-07 14:04:06 +0800

    UI SO NEW

--- Stok/SO-New.xaml.cs
+++ Stok/SO-New.xaml.cs
@@ -5,5 +5,11 @@ public partial class SO_New : ContentPage
 	public SO_New()
 	{
 		InitializeComponent();
+        HiddenDatePicker.MinimumDate = DateTime.Today; // Kunci agar tidak boleh kurang dari sekarang
 	}
+
+    private void HiddenDatePicker_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        FormStartDate.Text = $"{e.NewDate:dd/MM/yyyy}";
+    }
 }
