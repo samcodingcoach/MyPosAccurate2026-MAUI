7199546e58d1608df0152969b17ee5f8b71b7995
samsu007 <itdev.nuansa@gmail.com>
2026-07-10 15:08:56 +0800

    Fix

--- Stok/SO-ResultAdd-Detail.xaml.cs
+++ Stok/SO-ResultAdd-Detail.xaml.cs
@@ -1,3 +1,5 @@
+using ZXing.Net.Maui;
+
 namespace MyPosAccurate2026.Stok;
 
 public partial class SO_ResultAdd_Detail : ContentPage
@@ -6,4 +8,19 @@ public partial class SO_ResultAdd_Detail : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void CameraScanner_BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
+    {
+        // Logika pemindaian kode batang dapat ditambahkan di sini
+    }
+
+    private void BtnViewSerial_Clicked(object sender, EventArgs e)
+    {
+        ViewSerialInput.IsVisible = true;
+    }
+
+    private void BtnSimpan_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
